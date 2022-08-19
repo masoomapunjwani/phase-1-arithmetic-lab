6ab37c7e2083528f774223dcb2953e52f7fced9a
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-19 14:02:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//random equation
+let random = Math.floor(Math.random() * 10) + 1;
+
+//mod function
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod);
+
+//max function
+let max = Math.max(1,4,7,20);
+
+
